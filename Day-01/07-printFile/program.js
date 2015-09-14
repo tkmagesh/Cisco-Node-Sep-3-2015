4d761fa0c61094e99795b155cf810abcc36f468a
tkmagesh <tkmagesh77@gmail.com>
2015-09-14 14:49:45 +0530

    printFle

---
+++ Day-01/07-printFile/program.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+var filename = process.argv[2];
+if (!filename) {
+    console.log('invalid arguments');
+    process.exit(1);
+}
+if (!fs.existsSync(filename)){
+    console.log('invalid filename');
+    process.exit(1);
+}
+/*
+var fileContents = fs.readFileSync(filename, {encoding : 'utf8'});
+console.log(fileContents);
+console.log("done");
+*/
+
+fs.readFile(filename, {encoding : 'utf8'}, function(err, fileContents){
+    if (err){
+        console.log(err);
+        process.exit(1);
+    };
+    console.log(fileContents);
+    console.log("done");
+});

daf175442426e020388b88bd031167edd35ff40f
tkmagesh <tkmagesh77@gmail.com>
2015-09-15 16:43:22 +0530

    Day-2 Complete

--- Day-02/14-taskmanager/middlewares/session.js
+++ Day-02/14-taskmanager/middlewares/session.js
@@ -1,15 +1,35 @@
 var uuid = require('node-uuid');
 var sessionStore = {};
-module.exports = function(req, res, next){
-    var sessionId = req.cookies.sessionId;
 
+
+function removeOrphanedSessions(){
+    for(var key in sessionStore){
+        var sessionObject = sessionStore[key];
+        var elapsedTime = new Date() - sessionObject.lastAccessedTime;
+        if (elapsedTime >= 60000){
+            console.log("session with id ", key, " is being cleared");
+            delete sessionStore[key];
+        }
+    }
+}
+
+setInterval(removeOrphanedSessions, 20000);
+
+module.exports = function(req, res, next){
+
+    var sessionId = req.cookies.sessionId;
     if (sessionId && sessionStore[sessionId]){
-        req.session = sessionStore[sessionId];
+        sessionStore[sessionId].lastAccessedTime = new Date();
+        req.session = sessionStore[sessionId].data;
+
     } else {
         sessionId = uuid.v1();
-        sessionStore[sessionId] = {};
+        sessionStore[sessionId] = {
+            data : {},
+            lastAccessedTime: new Date()
+        };
         res.cookie('sessionId', sessionId);
-        req.session = sessionStore[sessionId];
+        req.session = sessionStore[sessionId].data;
     }
     next();
 }
